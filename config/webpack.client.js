20c797ed0cb5dfa9cc04da1ec441ef3b0a8d3841
munaibh <munaibh@gmail.com>
2020-03-02 22:00:12 +0000

    We should not clean "favicon.png".

--- config/webpack.client.js
+++ config/webpack.client.js
@@ -9,7 +9,7 @@ module.exports = (env, _argv) => {
   const entries = env.production ? [] : ['./config/plugins/HotLoader/subscribe']
   const filename = env.production ? '[name].[contenthash:8]' : '[name]'
   const extract = { loader: MiniCssExtractPlugin.loader, options: { hmr: !env.production } }
-  const cleanWhiteList = ['!favicon.ico']
+  const cleanWhiteList = ['!favicon.png']
 
   return {
     mode: environment,
