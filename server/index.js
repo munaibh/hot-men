80db30fa27316dadf9ccb7632bbf04d920f891f4
munaibh <munaibh@gmail.com>
2020-03-02 20:23:06 +0000

    Accept HMR on entry and server modules.

--- server/index.js
+++ server/index.js
@@ -5,3 +5,17 @@ let currentApp = require('./server').default
 let server = http.createServer(currentApp).listen(PORT, function () {
   console.info(`Server started on: http://localhost:${PORT}`)
 })
+
+if (module.hot) {
+  module.hot.accept("./server", () => {
+    server.removeListener("request", currentApp)
+    currentApp = require('./server').default
+    server.on("request", currentApp)
+  })
+  
+  module.hot.accept(err => console.log(err))
+  module.hot.dispose(_ => {
+    console.log('Disposing entry module...')
+    server.close()
+  })
+}
