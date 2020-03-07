f58a7f7c0b6c2eb1ba5957b8f07f75f7094e3e37
munaibh <munaibh@gmail.com>
2020-03-07 13:37:36 +0000

    Heroku doesn't have git info, so fallback to package version.

--- config/plugins/WebpackPluginUtils.js
+++ config/plugins/WebpackPluginUtils.js
@@ -1,14 +1,21 @@
+const path = require('path')
+
 const PluginUtils = function()  {
-  let cachedVersion = process.env.HEROKU_RELEASE_VERSION
+  let cachedVersion
   
   const tryWithFallback = (callback, fallback)  => {
     try { return callback() }
     catch(e) { return fallback }
   }
+
+  const packageVersion = () => {
+    var packageJSON = require('../../package.json')
+    return packageJSON.version || '0.0.0-dev'
+  }
   
   const version = () => {
     if(cachedVersion) return cachedVersion
-    const fallbackTag = '0.0.0-dev'
+    const fallbackTag = packageVersion()
     const execDescribe = _ => cp.execSync('git describe --tags --dirty --abbrev=0', {cwd: '.'})
     const lastTag = tryWithFallback(execDescribe, fallbackTag).toString()
     return cachedVersion = lastTag.replace(/-/g, '_').trim()
