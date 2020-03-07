119e7cd3be9f282e98a4801685a549771b5b7c08
munaibh <munaibh@gmail.com>
2020-03-07 23:13:51 +0000

    Basic service worker implementation (offline page and cache first).

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,2 +1,20 @@
-const staticCacheName = __VERSION__
-const staticCacheManifest = __MANIFEST__
+const CACHE_NAME = __VERSION__
+const MANIFEST = __MANIFEST__
+
+const urlsToCache = ['/offline.html']
+
+self.addEventListener("install", async event => {
+  const cache = await caches.open(CACHE_NAME)
+  return cache.addAll(urlsToCache)
+})
+
+self.addEventListener("fetch", e => {
+  e.respondWith(getResponseByRequest(e.request))
+})
+
+const getResponseByRequest = async request => {
+  const cache = await caches.open(CACHE_NAME)
+  const cachedResponse = await cache.match(request)
+  return cachedResponse || fetch(request)
+    .catch(err => caches.match('/offline.html'))
+}
