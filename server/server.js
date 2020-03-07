402c59997fa7baed0eab8403f5a83de3fbff5546
munaibh <munaibh@gmail.com>
2020-03-07 15:47:55 +0000

    Make use of "compression" package to enable gzipping of assets.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import helpers from './helpers'
+import compression  from 'compression'
 
 const app = express()
 
@@ -8,6 +9,7 @@ app.set('views', path.join(__dirname, '..', 'views'))
 app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(helpers)
+app.use(compression())
 
 app.get('/', (req, res, next) => {
   res.render('index')
