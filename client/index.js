1072af677cf570839fda6f4f12ebb5f1dc068a60
munaibh <munaibh@gmail.com>
2020-09-20 13:54:10 +0100

    Import all images in `client/images` directory.

--- client/index.js
+++ client/index.js
@@ -1,3 +1,6 @@
+const importAll = (r) => r.keys().map(r)
+importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js');
 }
